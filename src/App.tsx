5a5d715553d69883c234e3bc7a803ab8d7cdb5cc
Satish Bandaru <satish.bandaru@truecaller.com>
2024-04-18 15:26:01 +0200

    Fix compilation errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import TaskList from './components/TaskList';
+import CustomizationOptions from './components/CustomizationOptions';
 import { TaskType } from './types';
-import { TaskSection } from './constants';
+import { FontOption, TaskSection } from './constants';
 import { DragDropContext, DropResult } from 'react-beautiful-dnd';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import RemoteSyncStorage from './components/RemoteSyncStorage';
 
 function App() {
   const [topTasks, setTopTasks] = useState<TaskType[]>([]);
   const [backlogTasks, setBacklogTasks] = useState<TaskType[]>([]);
-  const [newTaskText, setNewTaskText] = useState('');
+  const [theme, setTheme] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+  const [font, setFont] = useState(FontOption.ROBOTO);
+  const [isDefocusCollapsed, setIsDefocusCollapsed] = useState(() => {
+    const saved = localStorage.getItem('isDefocusCollapsed');
+    return saved === 'true';
+  });
 
   useEffect(() => {
     if (chrome.storage) {
-      chrome.storage.local.get(['topTasks', 'backlogTasks'], (result: { topTasks?: TaskType[]; backlogTasks?: TaskType[] }) => {
-        setTopTasks(result.topTasks || []);
-        setBacklogTasks(result.backlogTasks || []);
+      let syncHasTasks;
+      RemoteSyncStorage().getRemoteTasks().then((tasks) => {
+        setTopTasks(tasks[0]);
+        setBacklogTasks(tasks[1]);
+        syncHasTasks = tasks[0].length > 0 || tasks[1].length > 0;
       });
+
+      if (!syncHasTasks) {
+        chrome.storage.local.get(['topTasks', 'backlogTasks'], (result: { topTasks?: TaskType[]; backlogTasks?: TaskType[] }) => {
+          setTopTasks(result.topTasks || []);
+          setBacklogTasks(result.backlogTasks || []);
+        });
+      }
     }
+    chrome.storage.local.get('theme').then((result) => {
+      if (result.theme) {
+        setTheme(result.theme);
+      } else {
+        setTheme(theme);
+      }
+    });
   }, []);
+
+  useEffect(() => {
+    localStorage.setItem('isDefocusCollapsed', String(isDefocusCollapsed));
+  }, [isDefocusCollapsed]);
+
+  useEffect(() => {
+    chrome.storage.local.get(['selectedFont'], (result) => {
+      if (result.selectedFont && Object.values(FontOption).includes(result.selectedFont)) {
+        setFont(result.selectedFont);
+      }
+    });
+  }, []);
+
+  const handleFontChange = (selectedFont: FontOption) => {
+    setFont(selectedFont);
+    // Persist the selected font to chrome.storage
+    chrome.storage.local.set({ 'selectedFont': selectedFont }, () => {
+      console.log('Font is set to ' + selectedFont);
+    });
+  };
 
   const onToggleCompletion = (taskId: string) => {
     const newTopTasks = topTasks.map(task =>
@@ -30,6 +73,7 @@ function App() {
     setBacklogTasks(newBacklogTasks);
 
     chrome.storage.local.set({ topTasks: newTopTasks, backlogTasks: newBacklogTasks });
+    storeTasksToSyncStorage(newTopTasks, newBacklogTasks);
   };
 
   const onDelete = (taskId: string) => {
@@ -40,9 +84,14 @@ function App() {
     setBacklogTasks(newBacklogTasks);
 
     chrome.storage.local.set({ topTasks: newTopTasks, backlogTasks: newBacklogTasks });
+    storeTasksToSyncStorage(newTopTasks, newBacklogTasks);
   };
 
   const onEditFinished = (taskId: string, newText: string) => {
+    if (!newText.trim()) {
+      onDelete(taskId);
+      return;
+    }
     const updatedTopTasks = topTasks.map(task =>
       task.id === taskId ? { ...task, text: newText } : task);
     const updatedBacklogTasks = backlogTasks.map(task =>
@@ -52,6 +101,7 @@ function App() {
     setBacklogTasks(updatedBacklogTasks);
 
     chrome.storage.local.set({ topTasks: updatedTopTasks, backlogTasks: updatedBacklogTasks });
+    storeTasksToSyncStorage(updatedTopTasks, updatedBacklogTasks);
   };
 
   const onDragEnd = (result: DropResult) => {
@@ -61,36 +111,62 @@ function App() {
     }
 
     // Check if trying to add a fourth task to the Top tasks
-    if (destination.droppableId === TaskSection.TOP && topTasks.length >= 3 && source.droppableId !== TaskSection.TOP) {
-      toast.error("Only three top tasks allowed.");
+    if (destination.droppableId === TaskSection.FOCUS &&
+      topTasks.length >= 3 &&
+      source.droppableId !== TaskSection.FOCUS
+    ) {
+      toast.error(
+        "3's a crowd! Complete focus tasks to add more",
+        { autoClose: 2000, closeButton: false }
+      );
       return;
     }
 
-    // Determine if the drag is within the same list or between lists
-    const sourceTasks = source.droppableId === TaskSection.TOP ? topTasks : backlogTasks;
-    const destinationTasks = destination.droppableId === TaskSection.TOP ? topTasks : backlogTasks;
-
-    const [removed] = sourceTasks.splice(source.index, 1);
-
-    // If the drag is within the same list
-    if (source.droppableId === destination.droppableId) {
-      sourceTasks.splice(destination.index, 0, removed);
-    } else {
-      // If the drag is between lists
-      destinationTasks.splice(destination.index, 0, removed);
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
     }
 
-    // Update state based on the new order
-    if (source.droppableId === TaskSection.TOP) {
-      setTopTasks([...sourceTasks]);
-      setBacklogTasks([...destinationTasks]);
-    } else {
-      setTopTasks([...destinationTasks]);
-      setBacklogTasks([...sourceTasks]);
-    }
+    const start = source.droppableId === TaskSection.FOCUS ? topTasks : backlogTasks;
+    const finish = destination.droppableId === TaskSection.FOCUS ? topTasks : backlogTasks;
 
-    // Persist changes to storage
-    chrome.storage.local.set({ topTasks: topTasks, backlogTasks: backlogTasks });
+    // Moving items within the same list
+    if (start === finish) {
+      const newTasks = Array.from(start);
+      const [removed] = newTasks.splice(source.index, 1);
+      newTasks.splice(destination.index, 0, removed);
+
+      const updatedState = source.droppableId === TaskSection.FOCUS
+        ? { topTasks: newTasks }
+        : { backlogTasks: newTasks };
+
+      if (source.droppableId === TaskSection.FOCUS) {
+        setTopTasks(newTasks);
+      } else {
+        setBacklogTasks(newTasks);
+      }
+
+      // Persist changes to storage
+      chrome.storage.local.set(updatedState);
+    } else {
+      // Moving items between different lists
+      const startTasks = Array.from(start);
+      const finishTasks = Array.from(finish);
+      const [removed] = startTasks.splice(source.index, 1);
+      finishTasks.splice(destination.index, 0, removed);
+
+      const updatedState = source.droppableId === TaskSection.FOCUS
+        ? { topTasks: startTasks, backlogTasks: finishTasks }
+        : { topTasks: finishTasks, backlogTasks: startTasks };
+
+      setTopTasks(updatedState.topTasks);
+      setBacklogTasks(updatedState.backlogTasks);
+
+      // Persist changes to storage
+      chrome.storage.local.set(updatedState);
+    }
   };
 
   function generateUUID() {
@@ -103,7 +179,11 @@ function App() {
     return uuid;
   }
 
-  const addTask = () => {
+  function storeTasksToSyncStorage(topTasks: TaskType[], backlogTasks: TaskType[]) {
+    RemoteSyncStorage().updateRemoteTasks(topTasks, backlogTasks);
+  };
+
+  const addTask = (newTaskText: string, taskSection: TaskSection) => {
     if (!newTaskText.trim()) return;
     const newTask: TaskType = {
       id: generateUUID(),
@@ -112,54 +192,69 @@ function App() {
       createdDate: new Date().toISOString(),
     };
 
-    const updatedTopTasks = topTasks.length < 3 ? [...topTasks, newTask] : topTasks;
-    const updatedBacklogTasks = topTasks.length >= 3 ? [...backlogTasks, newTask] : backlogTasks;
-
-    setTopTasks(updatedTopTasks);
-    setBacklogTasks(updatedBacklogTasks);
-
-    setNewTaskText('');
-
-    if (chrome.storage) {
-      chrome.storage.local.set({ topTasks: updatedTopTasks, backlogTasks: updatedBacklogTasks });
+    if (taskSection === TaskSection.FOCUS) {
+      const updatedTopTasks = topTasks.length < 3 ? [...topTasks, newTask] : topTasks;
+      setTopTasks(updatedTopTasks);
+      if (chrome.storage) {
+        chrome.storage.local.set({ topTasks: updatedTopTasks });
+        storeTasksToSyncStorage(updatedTopTasks, backlogTasks);
+      }
+    } else if (taskSection === TaskSection.DEFOCUS) {
+      const updatedBacklogTasks = [...backlogTasks, newTask];
+      setBacklogTasks(updatedBacklogTasks);
+      if (chrome.storage) {
+        chrome.storage.local.set({ backlogTasks: updatedBacklogTasks });
+        storeTasksToSyncStorage(topTasks, updatedBacklogTasks);
+      }
     }
+  };
+
+  function calculateHeight(tasks: TaskType[]) {
+    const baseHeight = 10; // Minimal height when there are no tasks
+    const heightPerTask = 40; // Additional height per task
+    return `${baseHeight + (tasks.length * heightPerTask)}px`;
+  }
+
+  const toggleDefocusCollapse = () => {
+    setIsDefocusCollapsed(!isDefocusCollapsed);
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <div className="container">
-        <div className="task-section">
-          <h2>Top tasks</h2>
-          <TaskList
-            tasks={topTasks}
-            sectionId={TaskSection.TOP}
-            onToggleCompletion={onToggleCompletion}
-            onDelete={onDelete}
-            onEditFinished={onEditFinished}
-          />
-        </div>
-        <div className="task-section">
-          <h2>Backlog</h2>
-          <TaskList
-            tasks={backlogTasks}
-            sectionId={TaskSection.BACKLOG}
-            onToggleCompletion={onToggleCompletion}
-            onDelete={onDelete}
-            onEditFinished={onEditFinished}
-          />
-        </div>
-        <div className="add-task-container">
-          <input
-            type="text"
-            value={newTaskText}
-            onChange={(e) => setNewTaskText(e.target.value)}
-            onKeyDown={(e) => e.key === 'Enter' && addTask()}
-            className="add-task-input"
-            placeholder="Add new task"
-          />
-          <button onClick={addTask} className="add-task-button">+</button>
+      <div className={`container app ${theme}`} style={{ fontFamily: font }}>
+        <div className="body-container">
+          <div className="task-section" style={{ minHeight: calculateHeight(topTasks) }}>
+            <div className="task-list">
+              <TaskList
+                tasks={topTasks}
+                sectionId={TaskSection.FOCUS}
+                onToggleCompletion={onToggleCompletion}
+                onDelete={onDelete}
+                onEditFinished={onEditFinished}
+                theme={theme}
+                addTask={addTask}
+              />
+            </div>
+          </div>
+
+          <div className="task-section" style={{ minHeight: calculateHeight(backlogTasks) }}>
+            <div className="task-list">
+              <TaskList
+                tasks={backlogTasks}
+                sectionId={TaskSection.DEFOCUS}
+                onToggleCompletion={onToggleCompletion}
+                onDelete={onDelete}
+                onEditFinished={onEditFinished}
+                theme={theme}
+                isCollapsed={isDefocusCollapsed}
+                onToggleCollapsed={toggleDefocusCollapse}
+                addTask={addTask}
+              />
+            </div>
+          </div>
         </div>
       </div>
+      <CustomizationOptions currentFont={font} onFontChange={handleFontChange} />
       <ToastContainer position="bottom-center" />
     </DragDropContext>
   );
